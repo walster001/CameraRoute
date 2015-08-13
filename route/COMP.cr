70dc3d9967f7c10ff8512fd95fa34b3b07449172
walster001 <walster001@gmail.com>
2015-08-13 21:23:09 +0930

    Create COMP.cr

---
+++ route/COMP.cr
@@ -0,0 +1,19 @@
+sudo apt get "av.component"
+sudo apt get "hdmi.component"
+sudo apt get "comp.component"
+sudo apt get "vga.component"
+
+public class COMP {
+
+ if comp.component (/="1") return error null
+    else comp.component(connect.initiate);
+}
+
+private class lostConnection {
+if comp.component = "0" wait(2); then comp.component.reconnect;
+   }
+}
+
+private class purpleFix {
+if comp.component (res="4:3") puts (res="16:9);
+}
