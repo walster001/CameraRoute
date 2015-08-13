97a9c927d08c2641d0f978045920177b547cf298
walster001 <walster001@gmail.com>
2015-08-13 21:00:28 +0930

    Update AV.cr

--- route/AV.cr
+++ route/AV.cr
@@ -1,10 +1,15 @@
-sudo apt get import av.component
-sudo apt get import hdmi.component
-sudo apt get import comp.component
-sudo apt get import  vga.component
+sudo apt get "av.component"
+sudo apt get "hdmi.component"
+sudo apt get "comp.component"
+sudo apt get "vga.component"
 
-public void AV class {
+public class AV {
 
  if av.component (/="1") return error null
     else av.component(connect.initiate);
+}
+
+private class lostConnection {
+if av.component = "0" wait(2); then av.component.reconnect;
+   }
 }
