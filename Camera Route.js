d424ec3a00bed9ecf0e789a7f9ab29623cf89007
walster001 <walster001@gmail.com>
2015-08-13 21:20:12 +0930

    Update Camera Route.js

--- Camera Route.js
+++ Camera Route.js
@@ -1,1 +1,6 @@
-public void 
+public class main {
+  if adaptDisplay = "VGA" import "VGA.cr"
+  if adaptDisplay = "HDMI" import "HDMI.cr"
+  if adaptDisplay = "AV" import "AV.cr"
+  if adaptDisplay = "COMP" import "COMP.cr"
+}
